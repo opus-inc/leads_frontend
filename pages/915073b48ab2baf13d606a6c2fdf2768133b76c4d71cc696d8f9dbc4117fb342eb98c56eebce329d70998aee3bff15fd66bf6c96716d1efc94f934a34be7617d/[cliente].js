c59e459ae1f46f967c887f560422604c686db12c
scoorpionx <joto.x42@gmail.com>
2021-10-04 14:17:17 -0300

    #5 - página do cliente migrada

---
+++ pages/915073b48ab2baf13d606a6c2fdf2768133b76c4d71cc696d8f9dbc4117fb342eb98c56eebce329d70998aee3bff15fd66bf6c96716d1efc94f934a34be7617d/[cliente].js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { FormComponent } from "../../src/components/index";
+import { localApi } from "../../src/services/api";
+import Typography from "@material-ui/core/Typography";
+import translateLocal from "../../src/helpers/translateLocal";
+import { useRouter } from "next/router";
+
+const CadLeadStand = () => {
+  const router = useRouter();
+  const { cliente } = router.query;
+  const [form, setForm] = useState({
+    nome: "",
+    email: "",
+    telefone: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const campos = [
+    {
+      placeholder: "Insira o nome do cliente",
+      label: "Nome ",
+      name: "nome",
+      type: "text",
+      required: true,
+    },
+    {
+      placeholder: "Insira o e-mail do cliente",
+      label: "E-mail ",
+      name: "email",
+      type: "email",
+      required: true,
+    },
+    {
+      placeholder: "Insira o telefone do cliente",
+      label: "Telefone ",
+      name: "telefone",
+      type: "tel",
+      required: true,
+    },
+  ];
+  useEffect(() => {
+    document.title = "Cadastro Único";
+  }, []);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const { ok, originalError } = await localApi.post("/leads", {
+      ...form,
+      local: translateLocal[cliente], //
+      tipo: "Cliente",
+    });
+    if (!ok) {
+      alert(originalError.message);
+      setLoading(false);
+      return;
+    }
+    alert("Registro criado com sucesso!");
+    const temp = document.createElement("a");
+    temp.href = "https://opus.inc";
+    temp.click();
+    setLoading(false);
+  };
+
+  return (
+    <Wrapper>
+      <Typography
+        variant="h5"
+        component="div"
+        gutterBottom
+        style={{ marginBottom: "140px" }}
+      >
+        REALIZE SEU CADASTRO
+      </Typography>
+      <FormComponent
+        fields={campos}
+        onSubmit={onSubmit}
+        width={50}
+        state={form}
+        setState={setForm}
+        buttonText="Cadastrar"
+        loading={loading}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default CadLeadStand;
